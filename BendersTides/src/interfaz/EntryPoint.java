10e363819d5b2203ece0129d3c75f3d77ac7409a
jmarenco <jmarenco@208.91.112.55>
2026-02-04 15:36:38 -0300

    Version update

--- BendersTides/src/interfaz/EntryPoint.java
+++ BendersTides/src/interfaz/EntryPoint.java
@@ -7,7 +7,7 @@ import tides.BendersSolver;
 
 public class EntryPoint
 {
-	private static String _version = "0.07";
+	private static String _version = "0.10";
 	
 	public static void main(String[] args)
 	{
